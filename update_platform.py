e51cc752d841f861d5bfa39ff14d3a6cd3a41395
Austin de Coup-Crank <Austin.deCoupCrank@moodys.com>
2021-12-01 12:55:37 -0600

    black

--- update_platform.py
+++ update_platform.py
@@ -14,57 +14,63 @@ handler.setFormatter(formatter)
 root_log.addHandler(handler)
 
 args = {
-    'host': config.netbox_host,
-    'auth_token': config.netbox_token,
-    'use_ssl': True,
-    'ssl_verify': True,
+    "host": config.netbox_host,
+    "auth_token": config.netbox_token,
+    "use_ssl": True,
+    "ssl_verify": True,
 }
 
 netbox = NetBox(**args)
 platforms = netbox.dcim.get_platforms()
-devices = netbox.dcim.get_devices(tag='network-ios-xe')
- 
+devices = netbox.dcim.get_devices(tag="network-wlc")
 tag_map = {
-    'Network-Arista': 'eos',
-    'Network-IOS': 'ios',
-    'Network-IOS-XE': 'ios_xe',
-    'Network-Juniper': 'junos',
-    'Network-NXOS': 'nxos',
-    'Network-Riverbed': 'riverbed',
-    'Network-WLC': 'aireos',
+    "Network-Arista": "eos",
+    "Network-IOS": "ios",
+    "Network-IOS-XE": "ios_xe",
+    "Network-Juniper": "junos",
+    "Network-NXOS": "nxos",
+    "Network-Riverbed": "riverbed",
+    "Network-WLC": "aireos",
 }
+
 
 def get_platform_id(platform_slug):
     for platform in platforms:
-        if platform['slug'] == platform_slug:
-            return platform['id']
+        if platform["slug"] == platform_slug:
+            return platform["id"]
 
 
 def verify_platform(device):
-    if device['platform']:
-        log.info(f'{device["name"]}: Platform already set to "{device["platform"]["slug"]}", doing nothing')
+    if device["platform"]:
+        log.info(
+            f'{device["name"]}: Platform already set to "{device["platform"]["slug"]}", doing nothing'
+        )
     else:
-        tag = device['tags'][0]
+        tag = device["tags"][0]
         log.debug(f'{device["name"]}: Tag: "{tag}"')
         platform_slug = tag_map.get(tag)
         log.debug(f'{device["name"]}: Platform slug: "{platform_slug}"')
         if platform_slug:
             platform_id = get_platform_id(platform_slug)
             log.debug(f'{device["name"]}: Platform ID: "{platform_id}"')
-            netbox.dcim.update_device_by_id(device_id=device['id'], platform=platform_id)
+            netbox.dcim.update_device_by_id(
+                device_id=device["id"], platform=platform_id
+            )
             log.info(f'{device["name"]}: Updated platform to "{platform_slug}"')
         else:
-            log.info(f'{device["name"]}: No platform mapped for tag {tag}, doing nothing')
+            log.info(
+                f'{device["name"]}: No platform mapped for tag {tag}, doing nothing'
+            )
+
 
 def verify_all_platforms(devices):
     with ThreadPoolExecutor(max_workers=config.max_workers) as executor:
         return executor.map(verify_platform, devices)
 
+
 def main():
     verify_all_platforms(devices)
-        
-   
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
